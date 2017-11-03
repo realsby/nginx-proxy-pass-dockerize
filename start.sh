7f8d78cf759220bd5ef357d16bbd17b5c415b6c1
Francesco Nistri <francesconistri84@gmail.com>
2017-11-03 16:16:39 +0100

    copy nginx.conf to /etc/nginx/nginx.conf because the command: "nginx -c /app/nginx.conf -s quit" continue to read the default configuration, trying to kill the process with pid find in "/var/run/nginx.pid"

--- start.sh
+++ start.sh
@@ -2,5 +2,5 @@
 
 python environment_template.py /app/nginx.template.conf /app/nginx.conf || exit 1
 echo "Starting nginx with the following configuration:"
-cat /app/nginx.conf
+cat /app/nginx.conf > /etc/nginx/nginx.conf
 nginx -c /app/nginx.conf
